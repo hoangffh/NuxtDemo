3a750fc74a94c69e0f24b3cc52d8be2cc3107136
hoangtn <hoangtn@heligate.com.vn>
2023-04-04 14:35:17 +0700

    add axios method

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    postcss: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
-      css: ['~/assets/css/main.css'],
-      plugins:['@/plugins/antd']
-})
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  css: ["~/assets/css/main.css"],
+  plugins: ["@/plugins/antd"],
+  runtimeConfig: {
+    public: {
+      baseApiURl: process.env.BASE_API_URl,
+    },
+  },
+});
