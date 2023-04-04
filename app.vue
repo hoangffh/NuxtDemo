f053ba1e0da5d7057db0c05cbd8bb2bb44016946
hoangffh <nhathoangx06@gmail.com>
2023-04-04 22:57:02 +0700

    add pages

--- app.vue
+++ app.vue
@@ -1,9 +1,5 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  <div :style="{ background: 'rgb(190, 200, 200)', padding: '26px 16px 16px' }">
-    <a-button type="primary" class="w-52" ghost>Primary</a-button>
-    <a-button ghost>Default</a-button>
-    <a-button type="dashed" ghost>Dashed</a-button>
-    <a-button type="primary" danger ghost>Danger</a-button>
+ <div>
+    <NuxtPage />
   </div>
 </template>
